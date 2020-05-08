bffe58293c3f6324e0185167423f666cd1c15fd4
cmdlinebeep <joelwigton@gmail.com>
2020-05-08 14:55:26 -0600

    Frontend tested working.  Updated Postman tokens for 24 hours expiration.

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
-/* @TODO replace with your variables
- * ensure all variables on this page match your project
+/* ensure all variables on this page match your project
  */
 
 export const environment = {
